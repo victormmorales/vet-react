2a47632845650cd8c71c368f847e79efbbb6e8b3
victormmorales <victor.morales@criptan.com>
2022-04-05 18:00:34 +0200

    form

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,29 @@
 const Formulario = () => {
   return (
-    <div>
-      <h2>Formulario</h2>
+    <div className="md:w-1/2 lg:w-2/5">
+      <h2 className="font-black text-3xl text-center">Seguimiento pacientes</h2>
+      <p className="text-lg mt-5 text-center mb-10">
+        Añadir mascota y{" "}
+        <span className="text-indigo-600 font-bold ">Administrarlos</span>
+      </p>
+
+      <form className="bg-white shadow-md rounded-lg py-10 px-5">
+        <div>
+          <label
+            htmlFor="mascota"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Nombre de tu mascota
+          </label>
+          <input
+            autoFocus
+            id="mascota"
+            type="text"
+            placeholder="Nombre de tu mascota"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+      </form>
     </div>
   );
 };
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,8 +1,8 @@
 const ListadoPacientes = () => {
   return (
-    <>
+    <div className="w-1/2 lg:w-3/5">
       <h2>ListadoPacientes</h2>
-    </>
+    </div>
   );
 };
 
